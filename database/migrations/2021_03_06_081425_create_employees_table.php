5391bb0a560fc411d71685730fefb916bb1a12ba
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-03-06 09:43:09 +0100

    Add Employees

---
+++ database/migrations/2021_03_06_081425_create_employees_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('linkedInUrl')->nullable();
+            $table->string('department');
+            $table->timestamps();
+
+            $table->foreign('department')
+                ->references('department')
+                ->on('departments');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('employee_id')->unique()->nullable();
+
+            $table->foreign('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->NullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
