19512842a6eb8b8620d2efba0844445dbf3628cc
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-03-07 23:21:29 +0100

    Update Navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,32 +1,61 @@
-<nav class="border-b-8 border-red-600 flex items-center justify-between flex-wrap bg-teal px-6 py-3">
-    <div x-data="{isOpen: false}" class="flex items-center flex-no-shrink text-red-600 mr-6">
-        <a href="#"> <img href="#" class="h-10" src="{{ asset("img/avans-log.png") }}">
-    </div>
-    <div class=" block lg:hidden " x-data="{showMenu : false}">
-        <button @click="isOpen = true" class="flex items-center px-3 py-2 border rounded text-red-600 border-red-600 hover:text-red-600 hover:border-black">
-            <i class="fas fa-bars"></i>
-        </button>
-    </div>
-    <div x-show="isOpen" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-            <a href="#" class="block mt-4 lg:inline-block text-xl lg:mt-0 text-red-600 hover:text-black mr-4">
-                <b>Home</b>
-            </a>
-            <a href="#" class="block mt-4 lg:inline-block text-xl lg:mt-0 text-red-600 hover:text-black mr-4">
-                <b>Gegevens</b>
-            </a>
-            @auth
-                <div class="sm:float-right inline-block">
-                    <a href="{{ route("#") }}" class="inline-block mt-4 text-xl leading-none text-red-600 border-red-600 hover:text-black "><b>Uitloggen</b></a>
-                    <a class="fas fa-user"></a>
-                </div>
-            @else
-                <div class="sm:float-right inline-block">
-                    <a href="/login" class="inline-block mt-4 text-xl lg:mt-0 leading-none text-red-600 border-red-600 hover:text-black "><b>Inloggen</b></a>
-                    <a class="fas fa-user fa-2x fa-" ></a>
-                </div>
-            @endauth
+<nav x-data="{ open: false }">
+    <div class="border-b-8 border-red-600 flex bg-white px-6 py-3">
+        <img href="{{ route('home') }}" class="h-10 mx-3" src="{{ asset("img/avans-log.png") }}" alt="Logo of Avans">
+        <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold">
+            <a href="{{ route('home') }}">{{ __('Home') }}</a>
         </div>
+        {{--TODO: Check if role is admin--}}
+        {{--@auth('admin')--}}
+        <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold hidden lg:block">
+            <a href="#">{{ __('Gegevens') }}</a>
+        </div>
+        {{--@endauth--}}
+        <div class="flex-auto"></div>
+        @guest()
+            <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold hidden lg:block">
+                <a href="{{ route('login') }}">Login</a>
+            </div>
+        @else
+            <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold hidden lg:block">
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+            </div>
 
+            <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold hidden lg:block">
+                <a href="#">{{ Auth::user()->email }}</a>
+            </div>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        @endguest
+        <div class="flex-none self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold lg:hidden">
+            <div @click="open = !open"
+                 class="focus:outline-none cursor-pointer"
+            >
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white w-full border-b-4 border-red-600" x-show="open">
+        {{--TODO: Check if role is admin--}}
+        {{--@auth('admin')--}}
+        <div class="self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold">
+            <a href="#">{{ __('Gegevens') }}</a>
+        </div>
+        {{--@endauth--}}
+        @guest()
+            <div class="self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold">
+                <a href="{{ route('login') }}">Login</a>
+            </div>
+        @else
+            <div class="self-center p-2 text-xl text-red-600 hover:text-red-800 font-bold">
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        @endguest
     </div>
 </nav>
