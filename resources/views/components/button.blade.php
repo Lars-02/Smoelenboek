038c423878bcdca222c726213b609fbeb3ffc737
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-03-09 16:04:41 +0100

    Update button.blade.php
    Use the color used on other components.

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @props(['type' => 'button', 'click' => ''])
 
-<button {{ $attributes->class(["bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none"]) }} type="{{ $type }}" @click="{{ $click }}">
+<button {{ $attributes->class(["bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded focus:outline-none"]) }} type="{{ $type }}" @click="{{ $click }}">
     {{ $slot }}
 </button>
