679c461ab7da3e76b85e590b4c5e1cee725356d2
Nick <Nickishero@gmail.com>
2021-03-09 15:02:11 +0100

    removed selectbox borders

--- resources/views/components/time.blade.php
+++ resources/views/components/time.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-2 p-5 w-40 bg-white rounded-lg shadow-xl">
     <div class="flex">
-        <select name="hours" class="appearance-none outline-none bg-transparent text-xl">
+        <select name="hours" class="appearance-none outline-none border-0 bg-transparent text-xl">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -15,13 +15,13 @@
             <option value="12">12</option>
         </select>
         <span class="text-xl mr-3">:</span>
-        <select name="minutes" class="bg-transparent text-xl appearance-none outline-none mr-4">
+        <select name="minutes" class="bg-transparent text-xl border-0 appearance-none outline-none mr-4">
             <option value="0">00</option>
             <option value="15">15</option>
             <option value="30">30</option>
             <option value="45">45</option>
         </select>
-        <select name="ampm" class="bg-transparent text-xl appearance-none outline-none">
+        <select name="ampm" class="bg-transparent text-xl border-0 appearance-none outline-none">
             <option value="am">AM</option>
             <option value="pm">PM</option>
         </select>
