b86e1b89d67df842e8e93ce6e3aa7a870f290458
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-03-09 12:49:44 +0100

    First page of register
    Finished first page of the register form

--- resources/views/employee/registerFirst.blade.php
+++ resources/views/employee/registerFirst.blade.php
@@ -1,17 +1,15 @@
 <x-layout>
     <div>
         <h1 class="text-5xl pl-10 pt-3 pb-2">Gegevens invullen</h1>
-        <div class="bg-red-600 w-3/12 h-3 rounded-2xl"></div>
+        <div class="bg-red-600 w-7/12 h-3 rounded-2xl"></div>
     </div>
-    <div class="w-9/12 mt-40 pl-40 flex-col">
-        <form>
-            <label class="text-3xl bg-red-600 rounded-xl text-white w-full">Voornaam</label>
-            <input value="Voornaam...">
-            <label class="text-3xl bg-red-600 rounded-xl text-white w-full">Achternaam (incl. Tussenvoegsels)</label>
-            <input value="Achternaam...">
-            <label class="text-3xl bg-red-600 rounded-xl text-white w-full">Telefoonnummer</label>
-            <input value="Telefoonnummer...">
-            <button type="submit">Volgende</button>
+    <div class="my-10">
+        <form class=" md:w-1/2 md:ml-20 px-3 mb-6 md:mb-0">
+            <x-input id="voornaam" type="text" icon="fas fa-user">Voornaam</x-input>
+            <x-input id="achternaam" type="text" icon="fas fa-user">Achternaam (Incl. Tussenvoegsel)</x-input>
+            <x-input id="telefoonnummer" type="text" icon="fas fa-phone-alt">Telefoonnummer</x-input>
+            <x-button>Terug</x-button>
+            <x-button>Doorgaan</x-button>
         </form>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('/employee/registerFirst', 'employee.registerFirst');
 Route::view('/employee/registerSecond', 'employee.registerSecond');
+Route::view('/employee/registerThird', 'employee.registerThird');
