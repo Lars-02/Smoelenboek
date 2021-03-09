feaa97e3ccee7e8ac30a05c3d930e7af88917d02
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-03-09 13:45:23 +0100

    Update Register 3
    temp push for a merge

---
+++ resources/views/employee/registerThird.blade.php
@@ -0,0 +1,42 @@
+<x-layout>
+    <div class="font-size:15px">
+        <div class="flex">
+            <x-button class="m-1">Ma</x-button>
+            <i class="fas fa-clock fa-2x"></i>
+            <select name="hours" class="bg-transparent text-lg appearance-none outline-none">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="1">13</option>
+                <option value="2">14</option>
+                <option value="3">15</option>
+                <option value="4">16</option>
+                <option value="5">17</option>
+                <option value="6">18</option>
+                <option value="7">19</option>
+                <option value="8">20</option>
+                <option value="9">21</option>
+                <option value="10">22</option>
+                <option value="11">23</option>
+                <option value="12">24</option>
+            </select>
+            <span class="text-xl mr-3">:</span>
+            <select name="minutes" class="bg-transparent text-lg appearance-none outline-none">
+                <option value="0">00</option>
+                <option value="30">15</option>
+                <option value="0">30</option>
+                <option value="30">45</option>
+            </select>
+            <x-button class="fas fa-plus"></x-button>
+        </div>
+    </div>
+</x-layout>
