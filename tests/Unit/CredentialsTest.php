2a636961dff9732d244b45f1a3229e1f4c1d5e56
Nick <Nickishero@gmail.com>
2021-03-12 15:56:00 +0100

    created credentialstest class

---
+++ tests/Unit/CredentialsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CredentialsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testLoggingInShouldWork()
+    {
+
+    }
+
+    public function testRegisteringShouldWork()
+    {
+
+    }
+
+
+}
