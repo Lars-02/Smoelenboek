18b5017a463f6fe9489bf2a72a018449f6793982
Luke <lukevandijk342014@gmail.com>
2021-03-17 00:22:21 +0100

    Template based on 'wachtwoord vergeten'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 <x-layout>
-        <x-card title="test data">
-            halloasdafaaaaaaaaaaaaaaa
-            aaaaaaaaaaa
-            aaa
+        <x-card title="Wachtwoord vergeten">
+            <div class="py-14">
+            <x-input id='email' type="email" icon="fas fa-envelope">E-mail adres</x-input>
+                <x-button>Wachtwoord resetten</x-button>
+            </div>
         </x-card>
     </div>
 </x-layout>
