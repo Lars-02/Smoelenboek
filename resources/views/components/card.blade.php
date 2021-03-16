a36b6242db3f60623a95dd139afe382d5afc9e9d
Luke <lukevandijk342014@gmail.com>
2021-03-17 00:21:50 +0100

    Responsiveness
    Small changes to make it look more like the wireframes. Also the title now centers on mobile screens to hide the underscore width adjustment.
    
    See if you'd like to incorporate this commit or not.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,13 +1,14 @@
-            bg-white rounded-lg border-4 border-red-700 shadow-md my-10">
-<div class="grid justify-items-center pt-5">
-    <div class="max-w-xs rounded overflow-hidden shadow-lg border-4 border-red-700 my-2 bg-white max-w-3xl ">
-        <div class="px-6 py-4 items-center">
-            <div class="font-bold text-xl mb-2">{{$title}}</div>
-            <div class="h-2.5 w-6/12 bg-red-700 rounded-full"></div>
-            <p class=" ">
+<div class="grid justify-items-center py-5">
+
+    <div class="w-11/12 sm:max-w-3xl overflow-hidden sm:shadow-lg rounded border-2 border-gray-500 bg-white ">
+        <div class="p-2.5 items-center">
+            <div class="tracking-wide text-xl text-center sm:text-left">{{$title}}</div>
+            <div class="h-2 w-full sm:w-6/12 bg-red-700 rounded-full"></div>
+            <div class="px-5 sm:px-16">
                 {{ $slot }}
-            </p>
+            </div>
         </div>
     </div>
+
 </div>
 
