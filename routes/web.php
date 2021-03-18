964ee099c4bacf842f047efaeee70be36a5c3806
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-03-18 13:06:33 +0100

    Revert "Merge branch 'dev' into feature/#54CreateView"
    This reverts commit 6381128d6ac636d140ab5ca3b907a24576c89a71, reversing
    changes made to 0fd3430ff5fed58da0a75445af7f82c311ea7d10.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\HomeController;
+use App\Models\Department;
+use App\Models\Expertise;
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Add a redirect to the main page with an error.
-Route::fallback(function () {
-    return route('/');
+Route::get('/employee/form', function () {
+    $departments = Department::All();
+    $roles = Role::All();
+    $expertises = Expertise::all();
+    return view('employee.form', compact('departments', 'roles', 'expertises'));
 });
