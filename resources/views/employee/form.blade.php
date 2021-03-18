0b7782127dc918f917ae90de50669e2b71cb861f
Luke <lukevandijk342014@gmail.com>
2021-03-18 23:30:12 +0100

    Update form.blade.php
    Moved the button to be more like the wireframes. As well as the title of the cards.
    
    Label naming changed.

--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -5,12 +5,8 @@
     <form action="/employee" method="POST">
         @csrf
         <div class="mt-6" x-data="{ currentTab: 'first' }">
-        <x-button @click="currentTab = 'first'">Personalia</x-button>
-        <x-button @click="currentTab = 'second'">Extra info</x-button>
-        <x-button @click="currentTab = 'third'">Werkdagen</x-button>
-
             <div x-show="currentTab === 'first'">
-                <x-card title="Personalia">
+                <x-card title="Gegevens invullen">
                     @if ($errors->any())
                         <div class="alert alert-danger">
                             <ul>
@@ -20,57 +16,67 @@
                             </ul>
                         </div>
                     @endif
-                <x-input type="text" name="email" id="email" icon="fas fa-envelope">E-mail</x-input>
-                <x-input type="text" name="firstname" id="firstname" icon="fas fa-user-circle">Naam</x-input>
-                <x-input type="text" name="lastname" id="lastname" icon="fas fa-user-circle">Achternaam</x-input>
-                <x-input type="tel" name="telephone" id="telephone" icon="fas fa-phone">Telefoonnummer</x-input>
+                    <x-input type="text" name="email" id="email" icon="fas fa-envelope">E-mail</x-input>
+                    <x-input type="text" name="firstname" id="firstname" icon="fas fa-user-circle">Voornaam</x-input>
+                    <x-input type="text" name="lastname" id="lastname" icon="fas fa-user-circle">Achternaam</x-input>
+                    <x-input type="tel" name="telephone" id="telephone" icon="fas fa-phone">Telefoonnummer</x-input>
+                    <div>
+                        <x-button @click="currentTab = 'second'">Doorgaan</x-button>
+                    </div>
                 </x-card>
             </div>
             <div x-show="currentTab === 'second'">
-                <x-card title="Extra info">
+                <x-card title="Gegevens invullen">
                     <div class="mb-8">
-                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Department</label>
-                    <select name="department" id="department" class="px-2.5 py-2.5 w-full rounded">
-                        <option disabled selected>Kies een departement...</option>
-                        @foreach($departments as $department)
-                            <option value="{{$department}}">{{$department}}</option>
-                        @endforeach
-                    </select>
+                        <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Department</label>
+                        <select name="department" id="department" class="px-2.5 py-2.5 w-full rounded">
+                            <option disabled selected>Kies een departement...</option>
+                            @foreach($departments as $department)
+                                <option value="{{$department}}">{{$department}}</option>
+                            @endforeach
+                        </select>
                     </div>
                     <x-select id="expertise" :options="$expertises">Expertise</x-select>
-                    <x-select id="role" :options="$roles">Rollen</x-select>
+                    <x-select id="role" :options="$roles">Rol</x-select>
+                    <div>
+                        <x-button @click="currentTab = 'first'">Terug</x-button>
+                        <x-button @click="currentTab = 'third'">Doorgaan</x-button>
+                    </div>
                 </x-card>
             </div>
             <div x-show="currentTab === 'third'">
-                <x-card title="Werkdagen">
-                <div>
+                <x-card title="Gegevens invullen">
                     <div>
-                        <label>Maandag</label><br>
-                        Van: <x-time name="monday[1][start_time]"/>
-                        Tot: <x-time name="monday[1][end_time]"/><br>
-                    </div><br>
+                        <div>
+                            <label>Maandag</label><br>
+                            Van: <x-time name="monday[1][start_time]"/>
+                            Tot: <x-time name="monday[1][end_time]"/><br>
+                        </div><br>
+                        <div>
+                            <label>Dinsdag</label><br>
+                            Van: <x-time name="tuesday[1][start_time]"/>
+                            Tot: <x-time name="tuesday[1][end_time]"/><br>
+                        </div><br>
+                        <div>
+                            <label>Woensdag</label><br>
+                            Van: <x-time name="wednesday[1][start_time]"/>
+                            Tot: <x-time name="wednesday[1][end_time]"/><br>
+                        </div><br>
+                        <div>
+                            <label>Donderdag</label><br>
+                            Van: <x-time name="thursday[1][start_time]"/>
+                            Tot: <x-time  name="thursday[1][end_time]"/><br>
+                        </div><br>
+                        <div>
+                            <label>Vrijdag</label><br>
+                            Van: <x-time name="friday[1][start_time]"/>
+                            Tot: <x-time  name="friday[1][end_time]"/><br>
+                        </div><br>
+                    </div>
                     <div>
-                        <label>Dinsdag</label><br>
-                        Van: <x-time name="tuesday[1][start_time]"/>
-                        Tot: <x-time name="tuesday[1][end_time]"/><br>
-                    </div><br>
-                    <div>
-                        <label>Woensdag</label><br>
-                        Van: <x-time name="wednesday[1][start_time]"/>
-                        Tot: <x-time name="wednesday[1][end_time]"/><br>
-                    </div><br>
-                    <div>
-                        <label>Donderdag</label><br>
-                        Van: <x-time name="thursday[1][start_time]"/>
-                        Tot: <x-time  name="thursday[1][end_time]"/><br>
-                    </div><br>
-                    <div>
-                        <label>Vrijdag</label><br>
-                        Van: <x-time name="friday[1][start_time]"/>
-                        Tot: <x-time  name="friday[1][end_time]"/><br>
-                    </div><br>
-                </div>
-                    <x-button type="submit">Afronden</x-button>
+                        <x-button @click="currentTab = 'second'">Terug</x-button>
+                        <x-button type="submit">Afronden</x-button>
+                    </div>
                 </x-card>
             </div>
         </div>
