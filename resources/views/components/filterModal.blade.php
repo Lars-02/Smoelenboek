076cec3361e01f7df054ee57c786e3222fbad476
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-03-30 03:09:54 +0200

    Edit FilterModal

--- resources/views/components/filterModal.blade.php
+++ resources/views/components/filterModal.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-red-700 rounded-md m-3 py-3 px-6">
+<div class="bg-red-700 rounded-md shadow-lg mb-4 py-3 px-6">
     <div class="text-white text-xl md:text2xl lg:text-3xl">
         {{ $title }}
     </div>
