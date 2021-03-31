79247163621df739b6699d372f405f5a4476ac9c
Nick <Nickishero@gmail.com>
2021-03-31 14:09:25 +0200

    🤩🤩🤩Text Size Bigger!🤩🤩👹 👺👀

--- resources/views/employee/profile.blade.php
+++ resources/views/employee/profile.blade.php
@@ -40,20 +40,20 @@
                     <div class="text-gray-700">
                         <div class="grid md:grid-cols-2 text-sm">
                             <div class="grid grid-cols-2">
-                                <div class="px-4 py-2 font-semibold">E-Mail</div>
-                                <div class="px-4 py-2">{{$user->email}}</div>
+                                <div class="break-words md:text-2xl font-semibold">E-mail</div>
+                                <div class="break-words md:text-2xl">{{$user->email}}</div>
                             </div>
                             <div class="grid grid-cols-2">
-                                <div class="px-4 py-2 font-semibold">Telefoon</div>
-                                <div class="px-4 py-2">{{$employee->phoneNumber}}</div>
+                                <div class="break-words md:text-2xl font-semibold">Telefoon</div>
+                                <div class="break-words md:text-2xl">{{$employee->phoneNumber}}</div>
                             </div>
                             <div class="grid grid-cols-2">
-                                <div class="px-4 py-2 font-semibold">Voornaam</div>
-                                <div class="px-4 py-2">{{$employee->firstname}}</div>
+                                <div class="break-words md:text-2xl font-semibold">Voornaam</div>
+                                <div class="break-words md:text-2xl">{{$employee->firstname}}</div>
                             </div>
                             <div class="grid grid-cols-2">
-                                <div class="px-4 py-2 font-semibold">Achternaam</div>
-                                <div class="px-4 py-2">{{$employee->lastname}}</div>
+                                <div class="break-words md:text-2xl font-semibold">Achternaam</div>
+                                <div class="break-words md:text-2xl">{{$employee->lastname}}</div>
                             </div>
                         </div>
                     </div>
@@ -61,45 +61,46 @@
 
                 <div x-show="tab === 'afdeling'" class="bg-white p-3 shadow-sm rounded-sm h-full ">
                     <h2 class="font-bold md:text-5xl mb-5">Afdeling</h2>
-                    {{$employee->department}}
+                    <p class="md:text-2xl">{{$employee->department}}</p>
                 </div>
 
                 <div x-show="tab === 'werktijden'" class="bg-white p-3 shadow-sm rounded-sm h-full ">
                     <h2 class="font-bold md:text-5xl mb-5">Werkdagen</h2>
                     @foreach($workHour as $results)
-                        {{$results->day}}
+                        <p class="md:text-2xl">{{$results->day}}</p>
                     @endforeach
                 </div>
 
                 <div x-show="tab === 'blokken'" class="bg-white p-3 shadow-sm rounded-sm h-full ">
                     <h2 class="font-bold md:text-5xl mb-5">Blokken</h2>
                     @foreach($employee->lectorate as $lectorate)
-                        <p>{{$lectorate->name}}</p>
+                        <p class="md:text-2xl">{{$lectorate->name}}</p>
                     @endforeach
                     @foreach($employee->hobby as $hobby)
-                        <p>Hobby: {{$hobby->name}}</p>
+                        <p class="md:text-2xl">Hobby: {{$hobby->name}}</p>
                     @endforeach
                     @foreach($employee->course as $course)
-                        <p>Course: {{$course->name}}</p>
+                        <p class="md:text-2xl">Course: {{$course->name}}</p>
                     @endforeach
                     @foreach($employee->minor as $minor)
-                        <p>Minor: {{$minor->name}}</p>
+                        <p class="md:text-2xl">Minor: {{$minor->name}}</p>
                     @endforeach
                     @foreach($employee->learningLine as $learningLine)
-                        <p>Leerlijn: {{$learningLine->name}}</p>
+                        <p class="md:text-2xl">Leerlijn: {{$learningLine->name}}</p>
                     @endforeach
                     @foreach($employee->expertise as $expertise)
-                        <p>Expertise: {{$expertise->name}}</p>
+                        <p class="md:text-2xl">Expertise: {{$expertise->name}}</p>
                     @endforeach
                 </div>
 
                 <div x-show="tab === 'socialmedia'" class="bg-white p-3 shadow-sm rounded-sm h-full ">
                     <h2 class="font-bold md:text-5xl mb-5">Sociale Media</h2>
-                    <a class="underline font-semibold text-red-700 hover:text-red-800 visited:text-red-600" href="{{$employee->linkedInUrl}}">Linked in</a>
+                    <a class="md:text-2xl underline font-semibold text-red-700 hover:text-red-800 visited:text-red-600" href="{{$employee->linkedInUrl}}">Linked in</a>
                 </div>
 
                 <!-- End of profile tab -->
             </div>
         </div>
     </div>
+    <script type="javascript">document.getElementsByClassName('w-7/12 border-red-700 border-4 rounded focus:bg-white focus:text-black')[0].click()</script>
 </x-layout>
