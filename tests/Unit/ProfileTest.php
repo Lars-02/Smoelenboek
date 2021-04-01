da074b186ccce33c1977b8367b855e9743d6322b
Luke <lukevandijk342014@gmail.com>
2021-04-01 22:42:04 +0200

    Added test_profile_can_be_rendered

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -2,17 +2,32 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Employee;
+use App\Models\User;
+use App\Models\WorkHour;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * An unit test for a singular profile page.
      *
-     * @return void
      */
-    public function test_example()
+
+    use RefreshDatabase;
+
+    //Assert if the view contains the right name and inherently renders
+    public function test_profile_view_can_be_rendered()
     {
-        $this->assertTrue(true);
+        $employee = Employee::factory()->make([
+            'department'=>'AFM'
+        ]);
+        $user = User::factory()->make();
+        $workHour = WorkHour::where('employee_id', 20)->get();
+
+        $view = $this->view('employee/profile', ['employee'=> $employee, 'user' => $user, 'workHour' => $workHour]);
+
+        $view->assertSee($employee->name);
     }
 }
