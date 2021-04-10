8231813caa0a9b598c2787ecc23e41f1ce4e436e
Seraphia <smjacobs@avans.nl>
2021-04-10 22:39:46 +0200

    refactor UserDataTests to UserDataTest

---
+++ tests/Unit/UserDataTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Models\Employee;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class UserDataTest extends TestCase
+{
+    use DatabaseMigrations, RefreshDatabase;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+}
