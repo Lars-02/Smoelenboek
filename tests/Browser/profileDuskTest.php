591e03229e6f36a3d445fc659d5ba0d8e80c556f
Luke <lukevandijk342014@gmail.com>
2021-04-10 14:02:08 +0200

    Testing setup and setup for testcase

--- tests/Browser/profileDuskTest.php
+++ tests/Browser/profileDuskTest.php
@@ -10,9 +10,7 @@ use Tests\DuskTestCase;
 class profileDuskTest extends DuskTestCase
 {
     /**
-     * A user cannot view the register page without being authenticated.
-     *
-     * @return void
+     * A user cannot view the profile page without being authenticated.
      */
     public function test_user_cannot_view_profile_page_when_not_authenticated()
     {
@@ -24,31 +22,58 @@ class profileDuskTest extends DuskTestCase
     }
 
     /**
-     * A user can see the registration page when authenticated into the system as an admin.
-     *
-     * @return void
+     * A user cannot view the profile overview page without being authenticated.
      */
-    public function test_user_can_view_register_page_when_authenticated()
+    public function test_user_cannot_view_profile_page_when_not_authenticated()
     {
         $this->browse(function ($browser) {
-            $user = User::find(21);
-            $browser->loginAs($user)
+            $browser->visit('http://127.0.0.1:8000/profile/testuser');
+            $url = $browser->driver->getCurrentURL();
+            $this->assertEquals('http://127.0.0.1:8000/login', $url);
+        });
+    }
+
+    /**
+     * A user can see the profile page when authenticated into the system as an admin.
+     */
+    public function test_user_can_view_profile_page_when_authenticated()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('http://127.0.0.1:8000/login')
+                ->type('email', 'test@avans.nl')
+                ->type('password', 'password')
+                ->press('Inloggen')
                 ->visit('/profile/testuser');
             $url = $browser->driver->getCurrentURL();
             $this->assertEquals('http://127.0.0.1:8000/profile/testuser', $url);
         });
     }
+
+    /**
+     * A user can see the profile overview page when authenticated into the system as an admin.
+     */
+    public function test_user_can_view_profile_page_when_authenticated()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('http://127.0.0.1:8000/login')
+                ->type('email', 'test@avans.nl')
+                ->type('password', 'password')
+                ->press('Inloggen')
+                ->visit('/profile/testuser');
+            $url = $browser->driver->getCurrentURL();
+            $this->assertEquals('http://127.0.0.1:8000/profile/testuser', $url);
+        });
+    }
+
     public function test_user_can_click_side_nav()
     {
-        $this->browse(function ($browser){
-            $user = User::find(21);
-            $browser->loginAs($user)
-                ->visit('/profile/')
-                    ->press('account')
-                    ->press('afdeling')
-                    ->press('werktijden')
-                    ->press('blokken')
-                    ->press('socialmedia');
+        $this->browse(function ($browser) {
+            $browser->visit('http://127.0.0.1:8000/login')
+                ->type('email', 'test@avans.nl')
+                ->type('password', 'password')
+                ->press('Inloggen')
+                ->visit('/profile/testuser');
+            $this->
         });
     }
 }
