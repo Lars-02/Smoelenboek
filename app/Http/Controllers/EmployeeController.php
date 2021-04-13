afa75e33f32b293ab4179f330368bbf914d13ced
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-04-13 23:11:31 +0200

    fix for displaying days in profile

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DayOfWeek;
 use App\Models\Department;
 use App\Models\Employee;
 use App\Models\Expertise;
@@ -56,7 +57,7 @@ class EmployeeController extends Controller
                     foreach (request($days[$i]) as $parent) {
                         if (!empty($parent["start_time"]) && !empty($parent["end_time"])) {
                             $workinghours = new WorkHour;
-                            $workinghours->day = DB::table('day_of_week')->where('day', $days[$i])->first()->id;
+                            $workinghours->day = DayOfWeek::where('day', $days[$i])->first()->id;
                             $workinghours->employee_id = $user->employee->id;
                             $workinghours->start_time = $parent["start_time"];
                             $workinghours->end_time = $parent["end_time"];
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DayOfWeek;
 use App\Models\Department;
 use App\Models\Employee;
 use App\Models\Expertise;
@@ -26,8 +27,14 @@ class ProfileController extends Controller {
 
             $user = User::find($employee->user_id);
 
+            $workDays = [];
             $workHour = WorkHour::where('employee_id', '=' , $employee->id)->get();
-
+            foreach($workHour as $workDay)
+            {
+                $day = DayOfWeek::where('id', $workDay->day)->first();
+                array_push($workDays, $day);
+            }
+            $workHour = $workDays; 
             return View::make('employee/profile', ['employee'=> $employee], ['user' => $user, 'workHour' => $workHour]);
         }
 
