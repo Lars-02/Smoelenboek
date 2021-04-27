25407608f6258746c5b62f75a9e38e2265c7a4c7
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-04-28 00:49:04 +0200

    Add Base for Filters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,35 @@
 <x-layout>
-    <div class="h-12"></div>
-    <div class="sm:flex sm:h-screen sm:overflow-hidden mb-2 mx-3 sm:mx-4 md:mx-5">
-        <div class="sm:w-1/3 xl:w-1/4 mr-0 sm:mr-3 sm:overflow-y-scroll scrollbar-hide">
-            <x-filterModal title="Filters 1">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
-            </x-filterModal>
-            <x-filterModal title="Filters 2">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
-            </x-filterModal>
-            <x-filterModal title="Filters 3">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
-            </x-filterModal>
-        </div>
-        <div class="flex-grow mb-4 overflow-x-hidden overflow-y-scroll scrollbar-hide">
-            <div class="grid gap-4 md:gap-6 xl:gap-8 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                @foreach($employees as $employee)
-                    <x-profilecard :employee="$employee"></x-profilecard>
-                @endforeach
+    <form id="filterForm" method="GET" action="{{ route('home') }}">
+        <div class="sm:mx-4 md:mx-5 my-4">
+            <div class="sm:w-1/3 xl:w-1/4">
+                <x-button type="submit">Apply</x-button>
             </div>
         </div>
-    </div>
+        <div class="sm:flex sm:h-screen sm:overflow-hidden mb-2 mx-3 sm:mx-4 md:mx-5">
+            <div class="sm:w-1/3 xl:w-1/4 mr-0 sm:mr-3 sm:overflow-y-scroll scrollbar-hide">
+                @csrf
+                <x-filterModal title="Cursus">
+                    @foreach($courses as $course)
+                        @if(isset($request->get("courses")[$course->id]))
+                            <x-filterSelector name="courses[{{ $course->id }}]" checked="true">
+                                {{ $course->name }}
+                            </x-filterSelector>
+                        @else
+                            <x-filterSelector name="courses[{{ $course->id }}]" checked="false">
+                                {{ $course->name }}
+                            </x-filterSelector>
+                        @endif
+                    @endforeach
+                </x-filterModal>
+            </div>
+            <div class="flex-grow mb-4 overflow-x-hidden overflow-y-scroll scrollbar-hide">
+                <div
+                    class="grid gap-4 md:gap-6 xl:gap-8 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    @foreach($employees as $employee)
+                        <x-profilecard :employee="$employee"></x-profilecard>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </form>
 </x-layout>
