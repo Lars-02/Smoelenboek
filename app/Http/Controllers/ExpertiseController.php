fe706043a53358433df43584c5cb4302f09ea061
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-04-28 15:21:11 +0200

    Revert "Merge branch 'dev' of https://github.com/Lars-02/Smoelenboek into dev"
    This reverts commit 9c9e7c09a02f9eead67919679057029eead2b457, reversing
    changes made to 4df6323381e3bee17d14f7c2a88f3969fc4e5c3f.

---
+++ app/Http/Controllers/ExpertiseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expertise;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ExpertiseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Expertise $expertise
+     * @return Response
+     */
+    public function show(Expertise $expertise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Expertise $expertise
+     * @return Response
+     */
+    public function edit(Expertise $expertise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Expertise $expertise
+     * @return Response
+     */
+    public function update(Request $request, Expertise $expertise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Expertise $expertise
+     * @return Response
+     */
+    public function destroy(Expertise $expertise)
+    {
+        //
+    }
+}
