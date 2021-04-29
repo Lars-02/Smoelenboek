10cc4f307014ab96f47776c4c7aa30275d5bef25
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-04-30 01:16:32 +0200

    update view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,29 +2,29 @@
     <div class="h-12"></div>
     <div class="sm:flex sm:h-screen sm:overflow-hidden mb-2 mx-3 sm:mx-4 md:mx-5">
         <div class="sm:w-1/3 xl:w-1/4 mr-0 sm:mr-3 sm:overflow-y-scroll scrollbar-hide">
-            <x-filterModal title="Filters 1">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
+            <x-filterModal title="Hobbies">
+                @foreach($hobbies as $hobby)
+                    <x-filterSelector name="{{ $hobby->name }}">{{ $hobby->name }}</x-filterSelector>
+                @endforeach
             </x-filterModal>
-            <x-filterModal title="Filters 2">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
+                @foreach($lectorates as $lectorate)
+                    <x-filterSelector name="{{ $lectorate->name }}">{{ $lectorate->name }}</x-filterSelector>
+                @endforeach
             </x-filterModal>
-            <x-filterModal title="Filters 3">
-                <x-filterSelector name="filter">Filter 1</x-filterSelector>
-                <x-filterSelector name="filter">Filter 2</x-filterSelector>
-                <x-filterSelector name="filter">Filter 3</x-filterSelector>
-                <x-filterSelector name="filter">Filter 4</x-filterSelector>
-                <x-filterSelector name="filter">Filter 5</x-filterSelector>
-                <x-filterSelector name="filter">Filter 6</x-filterSelector>
+            <x-filterModal title="minors">
+                @foreach($minors as $minor)
+                    <x-filterSelector name="{{ $minor->name }}">{{ $minor->name }}</x-filterSelector>
+                @endforeach
+            </x-filterModal>
+            <x-filterModal title="Expertises">
+                @foreach($expertises as $expertise)
+                    <x-filterSelector name="{{ $expertise->name }}">{{ $expertise->name }}</x-filterSelector>
+                @endforeach
+            </x-filterModal>
+            <x-filterModal title="Roles">
+                @foreach($roles as $role)
+                    <x-filterSelector name="{{ $role->name }}">{{ $role->name }}</x-filterSelector>
+                @endforeach
             </x-filterModal>
         </div>
         <div class="flex-grow mb-4 overflow-x-hidden overflow-y-scroll scrollbar-hide">
