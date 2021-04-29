cd4e467196b7344627eb0e191aae70057d865980
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-04-30 00:51:31 +0200

    Filters aangemaakt

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Models\EmployeeHobby;
+use App\Models\Expertise;
+use App\Models\Hobby;
+use App\Models\Lectorate;
+use App\Models\Minor;
+use App\Models\Role;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 
@@ -34,8 +40,82 @@ class HomeController extends Controller
             || is_null($employee->department)) {
             return redirect()->route('employee.create');
         }
-
         $employees = Employee::all();
-        return view('home', compact(["employees"]));
+        $hobbies = Hobby::all();
+        $lectorates = Lectorate::all();
+        $minors = Minor::all();
+        $expertises = Expertise::all();
+        $roles = Role::all();
+        return view('home', compact(["employees", "hobbies", "lectorates", "minors", "expertises", "roles"]));
     }
+    public static function filterHobby($option)
+    {
+        $option = Hobby::where('name', $option)->first();
+        $hobbies = EmployeeHobby::all();
+        $employees = [];
+
+        foreach($hobbies as $hobby)
+        {
+            if($hobby->hobby_id == $option->id)
+            {
+                $employee = Employee::find($hobby->employee_id);
+                array_push($employees, $employee);
+            }
+        }
+        return $employees;
+    }
+    public static function filterLectorate($option)
+    {
+        $option = Lectorate::where('name', $option)->first();
+        $lectorates = EmployeeLectorate::all();
+        $employees = [];
+
+        foreach($lectorates as $lectorate)
+        {
+            if($lectorate->lectorate_id == $option->id)
+            {
+                $employee = Employee::find($lectorate->employee_id);
+                array_push($employees, $employee);
+            }
+        }
+        return $employees;
+    }
+    public static function filterMinor($option)
+    {
+        $option = Minor::where('name', $option)->first();
+        $minors = EmployeeMinor::all();
+        $employees = [];
+
+        foreach($minors as $minor)
+        {
+            if($minor->minor_id == $option->id)
+            {
+                $employee = Employee::find($minor->employee_id);
+                array_push($employees, $employee);
+            }
+        }
+        return $employees;
+    }
+    public static function filterExpertise($option)
+    {
+        $option = Expertise::where('name', $option)->first();
+        $expertises = EmployeeExpertise::all();
+        $employees = [];
+
+        foreach($expertises as $expertise)
+        {
+            if($expertise->minor_id == $option->id)
+            {
+                $employee = Employee::find($expertise->employee_id);
+                array_push($employees, $employee);
+            }
+        }
+        return $employees;
+    }
+   public static function filterRole($option)
+   {
+       $roles = Role::where('name', $option)->first();
+       $employees = Employee::where('role', $roles->name)->get();
+       return $employees;
+   }
 }
