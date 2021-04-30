a513600ebe61f69f58a1952fb42335e4f33ceac9
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-01 00:33:31 +0200

    Add more strict validation rules

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -134,10 +134,10 @@ class EmployeeController extends Controller
     public function update(Request $request, Employee $employee)
     {
         request()->validate([
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'phoneNumber' => 'required',
-            'email' => 'required|email',
+            'firstname' => 'required|alpha|min:2|max:60',
+            'lastname' => 'required|alpha|min:2|max:60',
+            'phoneNumber' => 'required|regex:/^+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/i',
+            'email' => 'required|email:rfc,dns',
             'department' => 'required',
         ]);
 
