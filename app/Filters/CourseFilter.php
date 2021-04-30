5d3f087ba80992e59b34133c2edac4b36eed6cc9
JulianRebel <63709758+JulianRebel@users.noreply.github.com>
2021-04-30 16:51:39 +0200

    CourseFilter tests and functionality

--- app/Filters/CourseFilter.php
+++ app/Filters/CourseFilter.php
@@ -10,6 +10,19 @@ class CourseFilter extends Facade implements Filter
 {
     public function filter(Collection $employees, array $filters): Collection
     {
+        foreach ($employees as $employee) {
+
+            $forget = true;
+
+            if($employee->course->whereIn('id', array_keys($filters))->count() != 0){
+                $forget = false;
+            }
+
+            if ($forget) {
+                $employees->forget($employee->id - 1);
+            }
+        }
+
         return $employees;
     }
 }
--- tests/Unit/FilterTest.php
+++ tests/Unit/FilterTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Filters\CourseFilter;
 use App\Filters\RoleFilter;
 use App\Http\Controllers\HomeController;
 use App\Models\Department;
@@ -31,6 +32,7 @@ class FilterTest extends DuskTestCase
      * @return void
      */
     public function test_filter_employee_on_role(){
+        // The "Docent" role has an id of 7
         $roleId = array("7" => "on");
         $roleFilter = new RoleFilter();
         $employees = Employee::all();
@@ -58,5 +60,41 @@ class FilterTest extends DuskTestCase
         }
 
         $this->assertEquals($employees, $roleFilter->filter(Employee::all(), $roleId));
+    }
+
+    /**
+     * A test to filter employees with a specific course.
+     *
+     * @return void
+     */
+    public function test_filter_employee_on_course(){
+        // The "ICT" course has an id of 6
+        $courseId = array("6" => "on");
+        $courseFilter = new CourseFilter();
+        $employees = Employee::all();
+
+        $courses = DB::table('employee')
+            ->select('employee.id')
+            ->join('course_employee', 'employee.id', '=', "course_employee.employee_id")
+            ->join('course', 'course_employee.course_id', '=', 'course.id')
+            ->whereIn('course.id', array_keys($courseId))
+            ->get();
+
+        foreach ($employees as $employee) {
+
+            $forget = true;
+
+            foreach ($courses as $course) {
+                if ($course->id == $employee->course->id) {
+                    $forget = false;
+                }
+            }
+
+            if ($forget) {
+                $employees->forget($employee->id - 1);
+            }
+        }
+
+        $this->assertEquals($employees, $courseFilter->filter(Employee::all(), $courseId));
     }
 }
