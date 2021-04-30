213f2b0f48933f49a3aaa80157606a1b1a838170
Nick <Nickishero@gmail.com>
2021-04-30 18:42:03 +0200

    make labels foldable

--- resources/views/employee/editProfile.blade.php
+++ resources/views/employee/editProfile.blade.php
@@ -117,59 +117,71 @@
 
                             <h2 class="font-bold md:text-5xl mb-5">Overige zaken</h2>
                             <div class="grid md:grid-cols-2">
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Lectoraten:</label>
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Lectoraten:</label>
+                                    <div x-show.transition.duration.300ms="open">
                                         @foreach($lectorates as $lectorate)
                                             <div>
                                                 <input @if ($employee->lectorate->contains($lectorate->id)) checked @endif value="{{$lectorate->id}}" name="lectorates[]" type="checkbox">
                                                 <label>{{$lectorate->name}}</label>
                                             </div>
                                         @endforeach
+                                    </div>
                                 </div>
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Hobby's:</label>
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Hobby's:</label>
+                                    <div x-show.transition.duration.300ms="open">
                                         @foreach($hobbies as $hobby)
                                             <div>
                                                 <input @if ($employee->hobby->contains($hobby->id)) checked @endif value="{{$hobby->id}}" name="hobbies[]" type="checkbox">
                                                 <label>{{$hobby->name}}</label>
                                             </div>
                                         @endforeach
+                                    </div>
                                 </div>
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Cursussen:</label>
-                                    @foreach($courses as $course)
-                                        <div>
-                                            <input @if ($employee->course->contains($course->id)) checked @endif value="{{$course->id}}" name="courses[]" type="checkbox">
-                                            <label>{{$course->name}}</label>
-                                        </div>
-                                    @endforeach
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Cursussen:</label>
+                                    <div x-show.transition.duration.300ms="open">
+                                        @foreach($courses as $course)
+                                            <div>
+                                                <input @if ($employee->course->contains($course->id)) checked @endif value="{{$course->id}}" name="courses[]" type="checkbox">
+                                                <label>{{$course->name}}</label>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Leerlijnen:</label>
-                                    @foreach($learningLines as $learningLine)
-                                        <div>
-                                            <input @if ($employee->learningLine->contains($learningLine->id)) checked @endif value="{{$learningLine->id}}" name="learningLines[]" type="checkbox">
-                                            <label>{{$learningLine->name}}</label>
-                                        </div>
-                                    @endforeach
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Leerlijnen:</label>
+                                    <div x-show.transition.duration.300ms="open">
+                                        @foreach($learningLines as $learningLine)
+                                            <div>
+                                                <input @if ($employee->learningLine->contains($learningLine->id)) checked @endif value="{{$learningLine->id}}" name="learningLines[]" type="checkbox">
+                                                <label>{{$learningLine->name}}</label>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Expertises:</label>
-                                    @foreach($expertises as $expertise)
-                                        <div>
-                                            <input @if ($employee->expertises->contains($expertise->id)) checked @endif value="{{$expertise->id}}" name="expertises[]" type="checkbox">
-                                            <label>{{$expertise->name}}</label>
-                                        </div>
-                                    @endforeach
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Expertises:</label>
+                                    <div x-show.transition.duration.300ms="open">
+                                        @foreach($expertises as $expertise)
+                                            <div>
+                                                <input @if ($employee->expertises->contains($expertise->id)) checked @endif value="{{$expertise->id}}" name="expertises[]" type="checkbox">
+                                                <label>{{$expertise->name}}</label>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <div class="mb-5 md:pr-5">
-                                    <label class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Minoren:</label>
-                                    @foreach($minors as $minor)
-                                        <div>
-                                            <input @if ($employee->minor->contains($minor->id)) checked @endif value="{{$minor->id}}" name="minors[]" type="checkbox">
-                                            <label>{{$minor->name}}</label>
-                                        </div>
-                                    @endforeach
+                                <div class="mb-5 md:pr-5" x-data="{ open: false }">
+                                    <label @click="open = !open" class="mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-6/12 bg-red-700 rounded">Minoren:</label>
+                                    <div x-show.transition.duration.300ms="open">
+                                        @foreach($minors as $minor)
+                                            <div>
+                                                <input @if ($employee->minor->contains($minor->id)) checked @endif value="{{$minor->id}}" name="minors[]" type="checkbox">
+                                                <label>{{$minor->name}}</label>
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 </div>
                             </div>
                         </div>
