a90502eb2ea4fdb5b62c83df8dd4473f1346b95e
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-01 02:25:41 +0200

    Add Input Label Component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,5 @@
 <div class="mb-8">
-    @if(isset($slot))
-        <label class="text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium" for="{{$id}}">{{$slot}}</label><br/>
-    @endif
+    <x-input.label id="{{$id}}">{{$slot}}</x-input.label>
     <select
         id="{{$id}}"
         name="{{ $id }}"
