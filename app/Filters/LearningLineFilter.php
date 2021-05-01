0404716c0dbc9a53f6211442f5221d4a52df1ca0
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-05-01 11:44:35 +0200

    Update LearningLineFilter.php

--- app/Filters/LearningLineFilter.php
+++ app/Filters/LearningLineFilter.php
@@ -20,21 +20,5 @@ class LearningLineFilter extends Facade implements Filter
             if($forget) $employees->forget($employee->id - 1); 
         }
         return $employees;
-/*         $learningLinePivot = EmployeeLearningLine::all();
-        $employeesNew = new Collection();
-        foreach($employees as $employee)
-        {
-            foreach($learningLinePivot as $learningLine)
-            {
-                foreach($filters as $key => $value)
-                {
-                    if($learningLine->learning_line_id == $key && $learningLine->employee_id == $employee->id)
-                    {
-                        $employeesNew->prepend(Employee::find($employee->id));
-                    }
-                }
-            }
-        }
-        return $employeesNew; */
     }
 }
