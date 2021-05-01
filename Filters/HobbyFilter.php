694019172bdf7f33dca4aa6af3a95cb59dccbdbe
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-05-01 02:20:57 +0200

    Testcases
    all tests

---
+++ Filters/HobbyFilter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filters;
+
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Facade;
+
+class HobbyFilter extends Facade implements Filter
+{
+    public function filter(Collection $employees, array $filters): Collection
+    {
+        foreach ($employees as $employee) {
+
+            $forget = true;
+            if($employee->hobby->whereIn('id', array_keys($filters))->count() != 0){
+                $forget = false;
+            }
+
+            if ($forget) {
+                $employees->forget($employee->id - 1);
+            }
+        }
+
+        return $employees;
+    }
+}
