9f2cf309121a74a5a6a1cb175df5fc94cece7e83
Zakariakarboub <49561317+Zakariakarboub@users.noreply.github.com>
2021-05-02 15:17:54 +0200

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Filters\CourseFilter;
+use App\Filters\MinorFilter;
+use App\Filters\RoleFilter;
+use App\Filters\WorkDayFilter;
+use App\Models\Course;
+use App\Models\Department;
 use App\Models\Employee;
-use App\Models\EmployeeHobby;
 use App\Models\Expertise;
 use App\Models\Hobby;
+use App\Models\LearningLine;
 use App\Models\Lectorate;
 use App\Models\Minor;
 use App\Models\Role;
+use App\Models\WorkDay;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -29,77 +37,35 @@ class HomeController extends Controller
      *
      * @return Renderable|RedirectResponse
      */
-    public function index()
+    public function index(Request $request)
     {
+        $employees = Employee::all();
+
+        if (isset($request['Hobbies'])) {
+            $courseFilter = new HobbyFilter();
+            $employees = $courseFilter->filter($employees, $request['Hobbies']);
+        }
+
+        if (isset($request['Lectorates'])) {
+            $courseFilter = new LectorateFilter();
+            $employees = $courseFilter->filter($employees, $request['Lectorates']);
+        }
+
+        if (isset($request['expertises'])) {
+            $functionFilter = new ExpertiseFilter();
+            $employees = $functionFilter->filter($employees, $request['expertises']);
+        }
+
+        if (isset($request['Minors'])) {
+            $workDayFilter = new MinorFilter();
+            $employees = $workDayFilter->filter($employees, $request['Minors']);
+        }
+
         $employees = Employee::all();
         $hobbies = Hobby::all();
         $lectorates = Lectorate::all();
         $minors = Minor::all();
         $expertises = Expertise::all();
         return view('home', compact(["employees", "hobbies", "lectorates", "minors", "expertises"]));
-    }
-    public static function filterHobby($option)
-    {
-        $option = Hobby::where('name', $option)->first();
-        $hobbies = EmployeeHobby::all();
-        $employees = [];
-
-        foreach($hobbies as $hobby)
-        {
-            if($hobby->hobby_id == $option->id)
-            {
-                $employee = Employee::find($hobby->employee_id);
-                array_push($employees, $employee);
-            }
-        }
-        return $employees;
-    }
-    public static function filterLectorate($option)
-    {
-        $option = Lectorate::where('name', $option)->first();
-        $lectorates = EmployeeLectorate::all();
-        $employees = [];
-
-        foreach($lectorates as $lectorate)
-        {
-            if($lectorate->lectorate_id == $option->id)
-            {
-                $employee = Employee::find($lectorate->employee_id);
-                array_push($employees, $employee);
-            }
-        }
-        return $employees;
-    }
-    public static function filterMinor($option)
-    {
-        $option = Minor::where('name', $option)->first();
-        $minors = EmployeeMinor::all();
-        $employees = [];
-
-        foreach($minors as $minor)
-        {
-            if($minor->minor_id == $option->id)
-            {
-                $employee = Employee::find($minor->employee_id);
-                array_push($employees, $employee);
-            }
-        }
-        return $employees;
-    }
-    public static function filterExpertise($option)
-    {
-        $option = Expertise::where('name', $option)->first();
-        $expertises = EmployeeExpertise::all();
-        $employees = [];
-
-        foreach($expertises as $expertise)
-        {
-            if($expertise->minor_id == $option->id)
-            {
-                $employee = Employee::find($expertise->employee_id);
-                array_push($employees, $employee);
-            }
-        }
-        return $employees;
     }
 }
