0e9bba1def63d3f0bfcf5896b915dee705057825
Nick <Nickishero@gmail.com>
2021-05-03 22:58:37 +0200

    fix filter gets too width big

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
             </div>
         </div>
         <div class="sm:flex sm:h-screen sm:overflow-hidden mb-2 mx-3 sm:mx-4 md:mx-5">
-            <div class="sm:w-1/2 xl:w-1/3 mr-0 sm:mr-3 sm:overflow-y-scroll scrollbar-hide">
+            <div class="max-w-xs sm:w-1/2 xl:w-1/3 mr-0 sm:mr-3 sm:overflow-y-scroll scrollbar-hide">
                 @csrf
                 <x-filterModal title="Cursus">
                     @foreach($courses as $course)
@@ -71,8 +71,7 @@
                 </x-filterModal>
             </div>
             <div class="flex-grow mb-4 overflow-x-hidden overflow-y-scroll scrollbar-hide rounded-md pb-4">
-                <div
-                    class="grid gap-4 md:gap-6 xl:gap-8 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <div class="grid gap-4 md:gap-6 xl:gap-8 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                     @foreach($employees as $employee)
                         <x-profilecard :employee="$employee"></x-profilecard>
                     @endforeach
