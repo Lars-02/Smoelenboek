d014aad748436493ac6d0be68c1bb59dd6e8432e
Luke <lukevandijk342014@gmail.com>
2021-05-03 19:26:56 +0200

    Input alerts
    For sending back errors because of faulty input from the user.

--- resources/views/components/input/label.blade.php
+++ resources/views/components/input/label.blade.php
@@ -1,3 +1,4 @@
 @if(isset($slot))
-    <label class="text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium" for="{{$id}}">{{$slot}}</label><br/>
+    <label class="relative text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium" for="{{$id}}">{{$slot}}</label>
+    <span class="clear-both"></span>
 @endif
