5b07d6cc5cddc8449b7f23ea663ce8cb976f808d
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-05-04 12:50:46 +0200

    Revert "Input alerts (#94)"
    This reverts commit 2a2fb625e01d781293d2f63ed415919f5bb9d31c.

--- resources/views/components/input/label.blade.php
+++ resources/views/components/input/label.blade.php
@@ -1,4 +1,3 @@
 @if(isset($slot))
-    <label class="relative text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium" for="{{$id}}">{{$slot}}</label>
-    <span class="clear-both"></span>
+    <label class="text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium" for="{{$id}}">{{$slot}}</label><br/>
 @endif
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-
+            width: {
+                '14/12': '120%',
+            }
         },
     },
     variants: {
