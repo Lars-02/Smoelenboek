63d51366d6fdc7ab5b8dcf0dc019b7a2e47d998f
Nick <Nickishero@gmail.com>
2021-05-04 13:21:23 +0200

    Revert "route change"
    This reverts commit a69647ca8a1990d2505b226921f03ff4bc131617.

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/register', [RegisterController::class, 'index']);
         Route::post('/', [UserController::class, 'registerNewUser'])->name('registerNewUser');
 
-        Route::resource('employee', EmployeeController::class);
+        Route::resource('employee', EmployeeController::class)
+            ->only(['show']);
     });
 });
 
