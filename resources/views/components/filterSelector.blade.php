4e984b6ca63d573f0801d5f0d96c0ef538b373b5
Nick <Nickishero@gmail.com>
2021-05-05 18:31:36 +0200

    break-words instead of truncate in filter.

--- resources/views/components/filterSelector.blade.php
+++ resources/views/components/filterSelector.blade.php
@@ -1,7 +1,7 @@
 <div
     x-data="{ checked: {{ $checked }} }"
     @click="checked = !checked"
-    class="text-white text-sm sm:text-md md:text-lg lg:text-xl cursor-pointer select-none truncate">
+    class="text-white text-sm sm:text-md md:text-lg lg:text-xl cursor-pointer select-none break-words">
     <i class="far fa-square" x-show="!checked"></i>
     <i class="fas fa-check-square" x-show="checked"></i>
     <input name="{{ $name }}" type="checkbox" x-bind:checked="checked" class="hidden"/>
