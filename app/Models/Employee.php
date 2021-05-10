2be2757b171891259f597883edd941d2ecf708cf
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-10 10:46:10 +0200

    Add Timestamps to Employees

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,9 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-
-    public $timestamps = false;
-
+    
     protected $fillable = [
         'user_id',
         'firstname',
