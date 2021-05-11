c57d091908f333cb40fb383b6e7ca8160c6d7082
Nick <Nickishero@gmail.com>
2021-05-11 19:31:04 +0200

    Revert "removed a test"
    This reverts commit 0d8274933dc7dbce9b7bad9bd4ef2d6510c5560a.

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -18,5 +18,33 @@ class ProfileTest extends TestCase
      */
 
     use RefreshDatabase;
-    
+
+    //Assert if the view contains the right name and inherently renders
+    public function test_profile_view_can_be_rendered()
+    {
+        $user = User::factory()
+        ->create([
+        'id' => 1
+        ]);
+
+        $employee = Employee::factory()
+            ->create([
+            'user_id' => 1,
+            'firstname' => 'Test',
+            'lastname' => 'User',
+        ]);
+
+        $amount = 5;
+        $workHour = [];
+        for($i = 1; $i <= $amount; $i++)
+        {
+            $day = WorkDay::factory()->create();
+            array_push($workHour, DB::table('employee_work_day')->insert(['employee_id' => $employee->id, 'work_day_id' => $day->id]));
+        }
+
+        $view = $this->view('employee/profile', ['employee'=> $employee, 'user' => $user, 'workHour' => $workHour]);
+
+        $view->assertSee($employee->firstname);
+        $view->assertSee($employee->lastname);
+    }
 }
