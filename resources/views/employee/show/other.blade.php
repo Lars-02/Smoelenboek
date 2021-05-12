bab882cb87e9d714bb92f3a96e1959d55fda5ce1
Nick <Nickishero@gmail.com>
2021-05-12 12:23:14 +0200

    select-all select-none on profile/editprofile/footer/navbar

--- resources/views/employee/show/other.blade.php
+++ resources/views/employee/show/other.blade.php
@@ -1,19 +1,19 @@
-<h2 class="font-bold md:text-5xl mb-5">Blokken</h2>
+<h2 class="font-bold md:text-5xl mb-5 select-none">Blokken</h2>
 @foreach($employee->lectorates as $lectorate)
-    <p class="md:text-2xl">{{$lectorate->name}}</p>
+    <p class="md:text-2xl select-all">{{$lectorate->name}}</p>
 @endforeach
 @foreach($employee->hobbies as $hobby)
-    <p class="md:text-2xl">Hobby: {{$hobby->name}}</p>
+    <p class="md:text-2xl select-all">Hobby: {{$hobby->name}}</p>
 @endforeach
 @foreach($employee->courses as $course)
-    <p class="md:text-2xl">Course: {{$course->name}}</p>
+    <p class="md:text-2xl select-all">Course: {{$course->name}}</p>
 @endforeach
 @foreach($employee->minors as $minor)
-    <p class="md:text-2xl">Minor: {{$minor->name}}</p>
+    <p class="md:text-2xl select-all">Minor: {{$minor->name}}</p>
 @endforeach
 @foreach($employee->learningLines as $learningLine)
-    <p class="md:text-2xl">Leerlijn: {{$learningLine->name}}</p>
+    <p class="md:text-2xl select-all">Leerlijn: {{$learningLine->name}}</p>
 @endforeach
 @foreach($employee->expertises as $expertise)
-    <p class="md:text-2xl">Expertise: {{$expertise->name}}</p>
+    <p class="md:text-2xl select-all">Expertise: {{$expertise->name}}</p>
 @endforeach
