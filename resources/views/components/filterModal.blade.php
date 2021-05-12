6af14c794a21a129d5d3b708dcce2b789c8de9f1
Nick <Nickishero@gmail.com>
2021-05-12 12:34:31 +0200

    front page, login, email

--- resources/views/components/filterModal.blade.php
+++ resources/views/components/filterModal.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-red-700 rounded-md shadow-lg mb-4 py-3 px-6" x-data="{ open: false }">
-    <div class="text-white text-xl md:text2xl lg:text-3xl"  @click="open = !open">
+    <div class="select-none text-white text-xl md:text2xl lg:text-3xl"  @click="open = !open">
         {{ $title }}
     </div>
     <div x-show.transition.duration.300ms="open">
