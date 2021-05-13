8b1bfce7510823f7dffac1ec8adfc8ce7464cd2d
JulianRebel <63709758+JulianRebel@users.noreply.github.com>
2021-05-13 16:01:46 +0200

    Fixed issue and made icons responsive

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,11 +1,11 @@
 <div class="select-none mb-8">
     <x-input.label id="{{$id}}">{{$slot}}</x-input.label>
     <div>
-        <i class="select-none absolute pl-3 pt-4 text-gray-600 {{ $icon }}"></i>
+        <i class="select-none absolute pl-3 pt-3 sm:pt-4 text-gray-600 {{ $icon }}"></i>
         <input
             id="{{$id}}"
             placeholder="{{$slot}}..."
-            {{$attributes->class(['select-text relative text-xs sm:text-sm md:text-base lg:text-lg px-2.5 py-2.5 pl-8 w-full rounded border-gray-400 focus:border-gray-400 text-gray-600 focus:ring-0'])}}
+            {{$attributes->class(['select-text text-xs sm:text-sm md:text-base lg:text-lg px-2.5 py-2.5 pl-8 w-full rounded border-gray-400 focus:border-gray-400 text-gray-600 focus:ring-0'])}}
         />
     </div>
 </div>
