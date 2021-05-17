818cbaea8529da17edb4104dc6d3f991420bbb2e
Nick <Nickishero@gmail.com>
2021-05-17 16:47:13 +0200

    FIX whitespace

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
                     x-init="$refs.loading.classList.add('hidden');"
                     @resize.window="watchScreen()"
                 >
-                <div class="flex h-screen antialiased text-gray-900 bg-gray-100">
+                <div class="fixed z-40 flex h-screen antialiased text-gray-900 bg-gray-100">
                     <div x-ref="loading" class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-red-700">Loading..... </div>
 
                     <div x-show="isSidebarOpen"
