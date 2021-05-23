3f7497677e6acc25dc1adb9ce20bc19a7a40e124
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-23 16:15:57 +0200

    Reformat code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,52 +6,53 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
             @error('email')
-                    <x-input
-                        error="{{$message}}"
-                        icon="fas fa-user"
-                        type="email"
-                        id="email"
-                        name="email"
-                        value="{{ old('email') }}"
-                        autocomplete="email"
-                        required autofocus
-                        >{{ __('Email') }}
-                    </x-input>
-                @else
-                    <x-input
-                        icon="fas fa-user"
-                        type="email"
-                        id="email"
-                        name="email"
-                        value="{{ old('email') }}"
-                        autocomplete="email"
-                        required autofocus
-                        >{{ __('Email') }}
-                    </x-input>
-            @enderror
-
             <x-input
-                icon="fas fa-lock"
-                type="password"
-                id="password"
-                name="password"
-                autocomplete="password"
-                required
-            >{{ __('Wachtwoord') }}
+                error="{{$message}}"
+                icon="fas fa-user"
+                type="email"
+                id="email"
+                name="email"
+                value="{{ old('email') }}"
+                autocomplete="email"
+                required autofocus
+            >{{ __('Email') }}
             </x-input>
+            @else
+                <x-input
+                    icon="fas fa-user"
+                    type="email"
+                    id="email"
+                    name="email"
+                    value="{{ old('email') }}"
+                    autocomplete="email"
+                    required autofocus
+                >{{ __('Email') }}
+                </x-input>
+                @enderror
 
-            <div class="flex">
-                <div class="mr-4">
-                    <x-button type="submit">
-                        {{ __('Inloggen') }}
-                    </x-button>
+                <x-input
+                    icon="fas fa-lock"
+                    type="password"
+                    id="password"
+                    name="password"
+                    autocomplete="password"
+                    required
+                >{{ __('Wachtwoord') }}
+                </x-input>
+
+                <div class="flex">
+                    <div class="mr-4">
+                        <x-button type="submit">
+                            {{ __('Inloggen') }}
+                        </x-button>
+                    </div>
+                    <div class="self-end">
+                        <a href="{{ route('password.request') }}"
+                           class="select-none text-xs sm:text-sm md:text-base lg:text-lg text-blue-500 underline">
+                            {{ __('Wachtwoord vergeten?') }}
+                        </a>
+                    </div>
                 </div>
-                <div class="self-end">
-                    <a href="{{ route('password.request') }}" class="select-none text-xs sm:text-sm md:text-base lg:text-lg text-blue-500 underline">
-                        {{ __('Wachtwoord vergeten?') }}
-                    </a>
-                </div>
-            </div>
         </form>
     </x-card>
 </x-layout>
