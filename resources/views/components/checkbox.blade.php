d0af337423f1b645c4737591da0f6c9c73a05fac
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-05-23 14:47:28 +0200

    Fix/issue#87 (#154)
    * added old input to some input fields
    
    * added checkbox fix to stay checked when changing the value + work days input is saved

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -3,15 +3,22 @@
     <div class="h-32 mt-10 overflow-y-scroll overflow-x-hidden border-b-2">
         @foreach($options as $option => $value)
             <div>
-            <input @isset($employee) @if ($employee->$options->contains($value->id)) checked
-                    @endif @endisset
-                value="{{ $option }}" type="checkbox" name="{{$id}}[]">
+            <input @isset($employee) @if ($employee->$options->contains($value->id)) checked @endif @endisset
+                value="{{ $option }}" 
+                @if (old($id) !== null)
+                    @foreach(old($id) as $val)
+                        @if($val == $option)
+                            checked
+                        @endif
+                    @endforeach
+                @endif            
+                type="checkbox" name="{{$id}}[]">
                 <label>{{$value}}</label>
             </div>
         @endforeach
     </div>
     @if(isset($error))
-        <p class="relative mb-3 bg-red-200 relative text-red-500 py-3 px-3 rounded-lg clear-both">
+        <p class="relative mb-3 bg-red-200 text-red-500 py-3 px-3 rounded-lg clear-both">
             {{$error}}
         </p>
     @endif
