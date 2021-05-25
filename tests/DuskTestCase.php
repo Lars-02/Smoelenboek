5b5a7540503ed5dd0d3b8d41641537f4f5cc4590
serdart1 <55989826+serdart1@users.noreply.github.com>
2021-05-25 17:33:23 +0200

    Update DuskTestCase.php

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -35,8 +35,7 @@ abstract class DuskTestCase extends BaseTestCase
             '--window-size=1920,1080',
         ])->unless($this->hasHeadlessDisabled(), function ($items) {
             return $items->merge([
-                //  '--headless',
-                '--no-sandbox',
+                 '--headless',
             ]);
         })->all());
 
