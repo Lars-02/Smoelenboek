259cbd058d782cbde53e8c1bad9eade86b7d2e68
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-26 09:43:00 +0200

    Simplefy Checkbox Error

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,25 +1,24 @@
 <div class="select-none mb-8">
-    <label class="select-none text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium">{{$slot}}</label>
+    <label
+        class="select-none text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-red-700 rounded font-medium">{{$slot}}</label>
     <div class="h-32 mt-10 overflow-y-scroll overflow-x-hidden border-b-2">
         @foreach($options as $option => $value)
             <div>
-            <input @isset($employee) @if ($employee->$options->contains($value->id)) checked @endif @endisset
-                value="{{ $option }}" 
-                @if (old($id) !== null)
-                    @foreach(old($id) as $val)
-                        @if($val == $option)
-                            checked
-                        @endif
-                    @endforeach
-                @endif            
-                type="checkbox" name="{{$id}}[]">
+                <input @isset($employee) @if ($employee->$options->contains($value->id)) checked @endif @endisset
+                value="{{ $option }}"
+                       @if (old($id) !== null)
+                       @foreach(old($id) as $val)
+                       @if($val == $option)
+                       checked
+                       @endif
+                       @endforeach
+                       @endif
+                       type="checkbox" name="{{$id}}[]">
                 <label>{{$value}}</label>
             </div>
         @endforeach
     </div>
-    @if(isset($error))
-        <p class="relative mb-3 bg-red-200 text-red-500 py-3 px-3 rounded-lg clear-both">
-            {{$error}}
-        </p>
-    @endif
+    @error($name)
+    <span class="text-red-600">{{ $message }}</span>
+    @enderror
 </div>
