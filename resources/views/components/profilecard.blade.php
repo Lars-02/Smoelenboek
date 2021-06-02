b3014ddd1ab91e3bb5f13427782df84480e2f510
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-02 02:27:20 +0200

    Make cursor pointer on card

--- resources/views/components/profilecard.blade.php
+++ resources/views/components/profilecard.blade.php
@@ -1,8 +1,8 @@
-<div class="bg-white shadow-lg rounded-lg">
+<div class="bg-white shadow-lg rounded-lg cursor-pointer">
     <a href="{{ route('employee.show', ['employee' => $employee]) }}">
         <div class="relative h-52">
             <div class="absolute bottom-2 left-2 z-20 w-full">
-                <div class="text-white text-md lg:text-lg xl:text-xl 2xl:text-2xl font-medium select-all "
+                <div class="text-white text-md lg:text-lg xl:text-xl 2xl:text-2xl font-medium "
                      title="{{ $employee->fullname }}">{{ $employee->fullname }}</div>
                 <div class="h-2 w-2/3 bg-red-700 rounded-full my-1"></div>
             </div>
@@ -17,19 +17,19 @@
             @endisset
         </div>
         <div class="px-4 py-2">
-            <div class="text-gray-500 text-lg lg:text-xl xl:text-2xl truncate select-none"
+            <div class="text-gray-500 text-lg lg:text-xl xl:text-2xl truncate"
                  title="{{ $department." - ".$function }}">{{ $department." - ".$function }}</div>
             <div class="text-sm md:text-md xl:text-lg truncate">
-                <a class="ml-2 text-red-700 select-all"
+                <a class="ml-2 text-red-700"
                    title="{{ $employee->user->email }}">{{ $employee->user->email }}</a><br>
-                <a class="ml-2 text-red-700 select-all"
+                <a class="ml-2 text-red-700"
                    title="{{ $employee->phoneNumber }}">{{ $employee->phoneNumber }}</a>
                 <div class="my-1 text-gray-500">
                     <div class="select-none">Werkt op</div>
                     <div>
                         @foreach($allDays as $day)
                             <span
-                                class="ml-2 select-none {{ in_array($day, $workingDays) ? "select-all text-red-700 font-medium" : "" }}">
+                                class="ml-2 {{ in_array($day, $workingDays) ? "select-all text-red-700 font-medium" : "" }}">
                             {{ substr($day, 0, 2) }}
                         </span>
                         @endforeach
@@ -37,41 +37,41 @@
                 </div>
                 <div class="mb-3">
                     @isset($courses)
-                        <div class="text-gray-500 select-none">Cursessen</div>
+                        <div class="text-gray-500">Cursessen</div>
                         @foreach($courses as $course)
-                            <div class="ml-2 text-red-700 select-all" title="{{ $course }}">{{ $course }}</div>
+                            <div class="ml-2 text-red-700" title="{{ $course }}">{{ $course }}</div>
                         @endforeach
                     @endisset
                 </div>
                 <div class="mb-3">
                     @isset($expertises)
-                        <div class="text-gray-500 select-none">Expertise</div>
+                        <div class="text-gray-500">Expertise</div>
                         @foreach($expertises as $expertise)
-                            <div class="ml-2 text-red-700 select-all" title="{{ $expertise }}">{{ $expertise }}</div>
+                            <div class="ml-2 text-red-700" title="{{ $expertise }}">{{ $expertise }}</div>
                         @endforeach
                     @endisset
                 </div>
                 <div class="mb-3">
                     @isset($minors)
-                        <div class="text-gray-500 select-none">Minors</div>
+                        <div class="text-gray-500">Minors</div>
                         @foreach($minors as $minor)
-                            <div class="ml-2 text-red-700 select-all" title="{{ $minor }}">{{ $minor }}</div>
+                            <div class="ml-2 text-red-700" title="{{ $minor }}">{{ $minor }}</div>
                         @endforeach
                     @endisset
                 </div>
                 <div class="mb-3">
                     @isset($lectorates)
-                        <div class="text-gray-500 select-none">Leerlijnen</div>
+                        <div class="text-gray-500">Leerlijnen</div>
                         @foreach($lectorates as $lectorate)
-                            <div class="ml-2 text-red-700 select-all" title="{{ $lectorate }}">{{ $lectorate }}</div>
+                            <div class="ml-2 text-red-700" title="{{ $lectorate }}">{{ $lectorate }}</div>
                         @endforeach
                     @endisset
                 </div>
                 <div class="mb-3">
                     @isset($hobbies)
-                        <div class="text-gray-500 select-none">Hobbies</div>
+                        <div class="text-gray-500">Hobbies</div>
                         @foreach($hobbies as $hobby)
-                            <div class="ml-2 text-red-700 select-all" title="{{ $hobby }}">{{ $hobby }}</div>
+                            <div class="ml-2 text-red-700" title="{{ $hobby }}">{{ $hobby }}</div>
                         @endforeach
                     @endisset
                 </div>
