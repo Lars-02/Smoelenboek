453a75f6888d95e48065ae94d0ecf1d30479c750
smjacobs <49642825+smjacobs@users.noreply.github.com>
2021-06-06 16:47:34 +0200

    with alphinejs instead

--- resources/views/components/contextHelp.blade.php
+++ resources/views/components/contextHelp.blade.php
@@ -1,22 +1,17 @@
-<button id="openButton" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
+<div x-data="{ open: false }">
+    <button id="openButton" @click="open = true" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
 
-<div id="popUp"
-    class="fixed transform scale-0 transition-transform">
+    <div id="popUp"
+        class="fixed transform"
+        x-show="open"
+        @click.away="open = false"
+    >
 
-    <div class="p-5 bg-white border-2 border-black rounded-md"> 
-        <span class="m:text-sm md:text-base lg:text-lg">
-            {{$slot}}
-        </span>
-        <button id="closebutton" class="m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
+        <div class="p-5 bg-white border-2 border-black rounded-md"> 
+            <span class="m:text-sm md:text-base lg:text-lg">
+                {{$slot}}
+            </span>
+            <button id="closebutton" @click="open = false" class="m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
+        </div>
     </div>
 </div>
-
-
-<script> 
-    const button = document.getElementById('openButton')
-    const closebutton = document.getElementById('closebutton')
-    const PopUp = document.getElementById('popUp')
-
-    button.addEventListener('click',()=>PopUp.classList.add('scale-100'))
-    closebutton.addEventListener('click',()=>PopUp.classList.remove('scale-100'))
-</script>
