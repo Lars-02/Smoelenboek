61f73387666a229ed4688bbd9d13586fa827bcd2
smjacobs <49642825+smjacobs@users.noreply.github.com>
2021-06-06 17:47:54 +0200

    clean up component

--- resources/views/components/contextHelp.blade.php
+++ resources/views/components/contextHelp.blade.php
@@ -1,12 +1,6 @@
 <div x-data="{ open: false }">
     <button id="openButton" @click="open = true" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
-
-    <div id="popUp"
-        class="fixed transform"
-        x-show="open"
-        @click.away="open = false"
-    >
-
+    <div id="popUp" class="fixed transform" x-show="open" @click.away="open = false">
         <div class="p-5 bg-white border-2 border-black rounded-md"> 
             <span class="m:text-sm md:text-base lg:text-lg">
                 {{$slot}}
