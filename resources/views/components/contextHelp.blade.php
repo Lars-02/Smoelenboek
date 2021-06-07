3cb48162e643cd21953018f0a53dc2aeee6aa9d8
smjacobs <49642825+smjacobs@users.noreply.github.com>
2021-06-07 10:12:38 +0200

    button doesnt submit form anymore

--- resources/views/components/contextHelp.blade.php
+++ resources/views/components/contextHelp.blade.php
@@ -1,11 +1,11 @@
 <div x-data="{ open: false }">
-    <button id="openButton" @click="open = true" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
+    <button id="openButton" type="button" @click="open = true" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
     <div id="popUp" class="fixed transform" x-show="open" @click.away="open = false">
         <div class="p-5 bg-white border-2 border-black rounded-md"> 
             <span class="m:text-sm md:text-base lg:text-lg">
                 {{$slot}}
             </span>
-            <button id="closebutton" @click="open = false" class="m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
+            <button id="closebutton" type="button" @click="open = false" class="m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
         </div>
     </div>
 </div>
