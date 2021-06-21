6ff13a1d15893f902c1e23b08da8dde9142501d2
smjacobs <49642825+smjacobs@users.noreply.github.com>
2021-06-21 18:25:30 +0200

    close button fixed on top right corner

--- resources/views/components/contextHelp.blade.php
+++ resources/views/components/contextHelp.blade.php
@@ -1,11 +1,12 @@
 <div x-data="{ open: false }">
     <button id="openButton" type="button" @click="open = true" class="sm:text-sm md:text-base lg:text-lg pl-2 text-gray-600 hover:text-red-700 far fa-question-circle"></button>
     <div id="popUp" class="absolute fixed transform z-10" x-show="open" @click.away="open = false">
-        <div class="p-5 bg-white border-2 border-black rounded-md"> 
+        <div class="relative p-5 bg-white border-2 border-black rounded-md"> 
+            <button id="closebutton" type="button" @click="open = false" class=" absolute top-2 right-2 m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
             <span class="m:text-sm md:text-base lg:text-lg">
                 {{$slot}}
             </span>
-            <button id="closebutton" type="button" @click="open = false" class="m:text-sm md:text-base lg:text-lg text-gray-600 hover:text-red-700 far fa-times-circle"></button>
+           
         </div>
     </div>
 </div>
