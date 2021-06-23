de67a7d595d75b3e3bc4b7ff0805e71b383a0307
smjacobs <49642825+smjacobs@users.noreply.github.com>
2021-06-23 11:04:14 +0200

    fixed border on input field

--- resources/views/components/inputModal.blade.php
+++ resources/views/components/inputModal.blade.php
@@ -3,13 +3,27 @@
         <form method="POST" action="{{ $route }}">
             @csrf
             <span class="m:text-sm md:text-base lg:text-lg">
-                <x-input
+                {{-- <x-input
                 icon="fas fa-filter"
                 id={{$slot}}
                 name="name"
                 value="{{$slot}}"
             >{{$slot}}
-            </x-input>
+            </x-input> --}}
+
+            <div class="select-none mb-2">
+                <x-input.label id="{{$slot}}">{{$slot}}</x-input.label>
+                <div class="relative">
+                    <input
+                        id="{{$slot}}"
+                        placeholder="{{$slot}}..."
+                        {{$attributes->class(['select-text relative text-xs sm:text-sm md:text-base lg:text-lg px-2.5 py-2.5 pl-8 w-full rounded border-gray-400  border-2 focus:border-gray-400 text-gray-600 focus:ring-0'])}}
+                        value="{{$slot}}"
+                    />
+                    <i class="select-none absolute left-3 bottom-3 sm:bottom-4 text-gray-600 fas fa-filter"></i>
+                </div>
+            </div>
+            
             </span>
             <x-button id="opslaanbutton" type="submit" @click="open = false">Opslaan</x-button>
             <x-button id="annulerenbutton" @click="open = false">Annuleren</x-button>
